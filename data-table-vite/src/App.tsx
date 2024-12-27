45850c36a91c9e7b0c3c78f7624ed564d53edafd
Chad Harmon <crharmon259@gmail.com>
2024-12-27 08:45:07 -0500

    xstate stuff added

--- data-table-vite/src/App.tsx
+++ data-table-vite/src/App.tsx
@@ -1,15 +1,20 @@
 import { DataTable } from "@/components/data-table-components/data-table";
 import { columns } from "@/components/data-table-components/columns";
-import {sampleData} from "@/components/data-table-components/samplet-data";
+
 import { ThemeProvider } from "@/components/theme-provider"
+import { useSelector } from '@xstate/react';
+import { transactionsActor } from "./state/tranctionMachine";
 
 import './App.css'
 
 function App() {
+  transactionsActor.start()
+  transactionsActor.send({ type: 'data.start.read'})
+  const tableData = useSelector(transactionsActor, (snapshot) => snapshot.context.tableData);
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
      <div className="card">
-        <DataTable data={sampleData} columns={columns} />
+        <DataTable data={tableData} columns={columns} />
       </div>
     </ThemeProvider>
   )
