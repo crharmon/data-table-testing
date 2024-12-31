88e9d034cfd98cdcdf6cbfacc8c2db63daa99e66
Chad Harmon <crharmon259@gmail.com>
2024-12-31 08:55:50 -0500

    Formated tsx files

--- data-table-vite/src/App.tsx
+++ data-table-vite/src/App.tsx
@@ -1,24 +1,23 @@
 import { DataTable } from "@/components/data-table-components/data-table";
 import { columns } from "@/components/data-table-components/columns";
 
-import { ThemeProvider } from "@/components/theme-provider"
-import { useSelector } from '@xstate/react';
+import { ThemeProvider } from "@/components/theme-provider";
+import { useSelector } from "@xstate/react";
 import { transactionsActor } from "./state/tranctionMachine";
-import { sampleData } from '@/components/data-table-components/samplet-data';
+import { sampleData } from "@/components/data-table-components/samplet-data";
 
-
-import './App.css'
+import "./App.css";
 import { Button } from "./components/ui/button";
 import { useParams } from "react-router-dom";
 
 // tip: optimize selectors by defining them externally when possible
-const selectTableData = (snapshot:any) => snapshot.context.tableData;
-const selectAssigned = (snapshot:any) => snapshot.context.assignedCalled;
+const selectTableData = (snapshot: any) => snapshot.context.tableData;
+const selectAssigned = (snapshot: any) => snapshot.context.assignedCalled;
 const handleClick = () => {
-  transactionsActor.send({ 
-    type: 'data.start.update',
+  transactionsActor.send({
+    type: "data.start.update",
     tableData: sampleData,
-  })
+  });
 };
 function App() {
   const urlParams = useParams<{ uiname: string }>();
@@ -27,18 +26,18 @@ function App() {
   // We can subscribe to an actor to be notified whenever its state changes
   const tableData = useSelector(transactionsActor, selectTableData);
   const aState = useSelector(transactionsActor, selectAssigned);
-  
-  transactionsActor.start()
+
+  transactionsActor.start();
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <div className="card">
+      <div className="card">
         <DataTable data={tableData} columns={columns} />
       </div>
-     <p>Current state: {JSON.stringify(aState)}</p>
-     <p>Current tableData: {JSON.stringify(tableData)}</p>
-     <Button onClick={handleClick}>Add Data</Button>
+      <p>Current state: {JSON.stringify(aState)}</p>
+      <p>Current tableData: {JSON.stringify(tableData)}</p>
+      <Button onClick={handleClick}>Add Data</Button>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- data-table-vite/src/state/tranctionMachine.ts
+++ data-table-vite/src/state/tranctionMachine.ts
@@ -1,12 +1,10 @@
-
-
-import { assign, createActor, setup } from 'xstate'
-import { Transaction } from '@/components/data-table-components/samplet-data';
+import { assign, createActor, setup } from "xstate";
+import { Transaction } from "@/components/data-table-components/samplet-data";
 const initialData: Transaction[] = [];
 export const machine = setup({
   actions: {
     startDataUpdate: assign({
-      tableData: ({ event }) => event.tableData
+      tableData: ({ event }) => event.tableData,
     }),
 
     eofReached: () => {
@@ -15,63 +13,56 @@ export const machine = setup({
     },
 
     errorReading: () => {
-       // Action when an error occurs
+      // Action when an error occurs
       console.log("Error occurred, returning to idle.");
-    }
-   },  
+    },
+  },
   types: {
     context: {
-        tableData : initialData,
+      tableData: initialData,
     },
     // TODO: Check if the event all need to have tableData
-    events: {} as 
-     {type: 'data.start.update',
-      tableData: Transaction[]
-      } |
-     {type: 'data.eof.reached', 
-      tableData: Transaction[]
-     } | 
-     {type: 'data.error.reading', 
-      tableData: Transaction[]
-     },
-  }
-})
-  .createMachine({
+    events: {} as
+      | { type: "data.start.update"; tableData: Transaction[] }
+      | { type: "data.eof.reached"; tableData: Transaction[] }
+      | { type: "data.error.reading"; tableData: Transaction[] },
+  },
+}).createMachine({
   context: {
     tableData: initialData,
-    assignedCalled: false
+    assignedCalled: false,
   },
-  "id": "dataRead",
-  initial: 'idle',
+  id: "dataRead",
+  initial: "idle",
   states: {
     idle: {
-      on: { 
-        // .send({ type: 'data.start.read'})
-        'data.start.update':{
-          target: "readingJson",
-          actions: 'startDataUpdate'
-      }
-        //"description": "The machine is in a waiting state, ready to start reading JSON data."
-    },
-    },
-    "readingJson": {
       on: {
-        'data.eof.reached': {
-          target: "readComplete",
-          actions: 'eofReached'  
-          },
-        "data.error.reading": {
-          "target": "idle",
-           actions: 'errorReading'
+        // .send({ type: 'data.start.read'})
+        "data.start.update": {
+          target: "readingJson",
+          actions: "startDataUpdate",
+        },
+        //"description": "The machine is in a waiting state, ready to start reading JSON data."
       },
-      //"description": "The machine is currently reading JSON data. It can transition to a final state if the end of file (EOF) is reached, or return to idle on an error."
     },
+    readingJson: {
+      on: {
+        "data.eof.reached": {
+          target: "readComplete",
+          actions: "eofReached",
+        },
+        "data.error.reading": {
+          target: "idle",
+          actions: "errorReading",
+        },
+        //"description": "The machine is currently reading JSON data. It can transition to a final state if the end of file (EOF) is reached, or return to idle on an error."
+      },
     },
-    "readComplete": {
-      "type": "final",
+    readComplete: {
+      type: "final",
       //"description": "The machine has successfully completed reading the JSON data. This is a final state."
-    }
-  }
-})
+    },
+  },
+});
 
-export const transactionsActor = createActor(machine)
+export const transactionsActor = createActor(machine);
